4039e5edd2dc91545e88c1bf3e05d073440a42f9
leetk77777 <leetk2495@naver.com>
2025-05-08 13:37:44 +0900

    npm 설치

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,25 @@
-// import firebase from "firebase/app";
-import { initializeApp } from 'firebase/app';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from 'firebase/database';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-    apiKey: "AIzaSyDmkMzG0foAOgnHdMpfYvBnpcsInNyzMiY",
-    authDomain: "react-slack-app-f3fc7.firebaseapp.com",
-    databaseURL: "https://react-slack-app-f3fc7-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "react-slack-app-f3fc7",
-    storageBucket: "react-slack-app-f3fc7.appspot.com",
-    messagingSenderId: "474862963570",
-    appId: "1:474862963570:web:a746c9cf57c4e2f85b2beb"
+const firebaseConfig = {
+  apiKey: "AIzaSyDXV7ay6XnxK-2s3O34AfSytkcDtlfg4RU",
+  authDomain: "react-chat-app-97edd.firebaseapp.com",
+  projectId: "react-chat-app-97edd",
+  storageBucket: "react-chat-app-97edd.firebasestorage.app",
+  messagingSenderId: "1065748018771",
+  appId: "1:1065748018771:web:b31e612c31e33abbc538a6",
+  measurementId: "G-RGVGK8SJZ9"
 };
 
 // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export default app;
 
--- src/pages/ChatPage/ChatPage.jsx
+++ src/pages/ChatPage/ChatPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import SidePanel from './SidePanel/SidePanel';
 import MainPanel from './MainPanel/MainPanel';
 
